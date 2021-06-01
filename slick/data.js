061bf0a52a4428dc4873322e04ce70b95ac69af7
Joshua DAVEY <932400863@bun2-206-03.acg.ac.nz>
2021-06-01 12:19:18 +1200

    tuesday

---
+++ slick/data.js
@@ -0,0 +1,15 @@
+var data = {
+  transport: {
+    vehicleType: 'Motorbike', 'Small Car', 'Large Car', 'Motor Home',
+    numPeople:  1, 2, 3, 4, 5, 6,
+    costs: 109, 129, 144, 200,
+    days: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'Days',
+    fuel: '3.7L/100km', '8.5L/100km', '9.7L/100km', '17L/100km',
+  },
+  accomodation: {
+    accomType: 'Hotel', 'Hostel', 'Motel', 'House',
+    numPeople: 1, 2, 3, 4,
+    costs: 157, 30, 90, 240,
+    nights: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'Nights',
+  }
+};
